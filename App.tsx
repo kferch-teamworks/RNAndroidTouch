e40c957bfa134aef468d6f20d33c6e1a941369ae
kferch-teamworks <kferch@teamworks.com>
2024-09-16 11:28:37 -0600

    npm install --global eas-cli and eas build:configure

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [inputText, setInputText] = useState("");
+  const [buttonPressCount, setButtonPressCount] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{ flex: 1, gap: 16, padding: 16, marginTop: 32 }}>
+      <Text style={{ fontWeight: "600"}}>React Native TextInput and Button</Text>
+      <TextInput style={{ borderWidth: 1, height: 40,  }} placeholder="Type something..." onChangeText={newText => setInputText(newText)}/>
+      <Text>input text is: {inputText}</Text>
+      <Button title="Press me" onPress={() => setButtonPressCount(buttonPressCount + 1)} />
+      <Text>button has been pressed {buttonPressCount} times.</Text>
       <StatusBar style="auto" />
     </View>
   );

20f061117884d82075f5d97823dbae7c91115ed0
kferch-teamworks <kferch@teamworks.com>
2024-09-16 13:52:54 -0600

    react native paper and textinput/button examples

--- App.tsx
+++ App.tsx
@@ -1,17 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button as NativeButton, StyleSheet, Text as NativeText, TextInput as NativeTextInput, View } from 'react-native';
+import { Button, Text, TextInput } from 'react-native-paper';
 
 export default function App() {
+  const [inputNativeText, setInputNativeText] = useState("");
+  const [nativeButtonPressCount, setNativeButtonPressCount] = useState(0);
   const [inputText, setInputText] = useState("");
   const [buttonPressCount, setButtonPressCount] = useState(0);
   return (
     <View style={{ flex: 1, gap: 16, padding: 16, marginTop: 32 }}>
-      <Text style={{ fontWeight: "600"}}>React Native TextInput and Button</Text>
-      <TextInput style={{ borderWidth: 1, height: 40,  }} placeholder="Type something..." onChangeText={newText => setInputText(newText)}/>
-      <Text>input text is: {inputText}</Text>
-      <Button title="Press me" onPress={() => setButtonPressCount(buttonPressCount + 1)} />
-      <Text>button has been pressed {buttonPressCount} times.</Text>
+      <View style={{ gap: 16, marginTop: 32 }}>
+        <NativeText style={{ fontWeight: "600"}}>React Native NativeTextInput and Button</NativeText>
+        <NativeTextInput style={{ borderWidth: 1, height: 40, paddingLeft: 8 }} placeholder="Type something..." onChangeText={newText => setInputNativeText(newText)}/>
+        <NativeText>input text is: {inputNativeText}</NativeText>
+        <NativeButton title="Press me" onPress={() => setNativeButtonPressCount(nativeButtonPressCount + 1)} />
+        <NativeText>button has been pressed {nativeButtonPressCount} times.</NativeText>
+      </View>
+      <View style={{ gap: 16, marginTop: 32 }}>
+        <Text style={{ fontWeight: "bold"}}>React Native Paper TextInput and Button</Text>
+        <TextInput style={{ borderWidth: 1, height: 40,  }} placeholder="Type something..." onChangeText={newText => setInputText(newText)}/>
+        <Text>input text is: {inputText}</Text>
+        <Button mode="contained" onPress={() => setButtonPressCount(buttonPressCount + 1)}>Press me</Button>
+        <Text>button has been pressed {buttonPressCount} times.</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
